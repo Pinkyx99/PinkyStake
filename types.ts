6f6eccbd6da2cada3d306708b9c4a0f4e1fa4627
Pinkyx99 <alijaramill@gmail.com>
2025-08-18 01:28:54 +0200

    Add files via upload

--- types.ts
+++ types.ts
@@ -1,12 +1,35 @@
 export interface Game {
   id: number;
   title: string;
-  slug?: 'chicken' | 'blackjack' | 'doors' | 'dice' | 'roulette' | 'crash' | 'limbo' | 'keno' | 'wheel' | 'pump' | 'flip';
+  slug?: 'chicken' | 'blackjack' | 'doors' | 'dice' | 'roulette' | 'crash' | 'limbo' | 'keno' | 'wheel' | 'pump' | 'flip' | 'mysterybox' | 'csgo';
   imageUrl: string;
   color: 'orange' | 'purple' | 'green' | 'brown' | 'yellow' | 'teal' | 'pink' | 'blue' | 'cyan' | 'red';
+}
+
+export interface BoxItem {
+  id: number;
+  name: string;
+  brand: string;
+  price: number;
+  imageUrl: string;
+  odds: number;
+}
+
+export interface InventoryItem extends BoxItem {
+  quantity: number;
 }
 
 export interface Profile {
   username: string;
   balance: number;
+  usedCodes: string[];
+  inventory: InventoryItem[];
+}
+
+export interface MysteryBox {
+  id: string;
+  name: string;
+  price: number;
+  imageUrl: string;
+  items: BoxItem[];
 }
